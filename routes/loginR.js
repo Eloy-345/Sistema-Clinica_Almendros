182e956eff77394b34f5e33be1ef2f2660797634
ClinicaAl <114184432+ClinicaAl@users.noreply.github.com>
2022-09-27 14:17:18 -0500

    Update loginR.js

--- routes/loginR.js
+++ routes/loginR.js
@@ -14,7 +14,7 @@ router.post('/auth', function (request, response) {
 		// Execute SQL query that'll select the account from the database based on the specified username and password
 		connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
 			// If there is an issue with the query, output the error
-			if (error) throw error;
+			//if (error) throw error;
 			// If the account exists
 			if (results.length > 0) {
 				// Authenticate the user
@@ -23,7 +23,6 @@ router.post('/auth', function (request, response) {
 				// Redirect to home page
 				response.redirect('/home1');
 			} else {
-				request.flash('danger', 'Flash is back!')
 				response.redirect('/');
 			}
 			response.end();
