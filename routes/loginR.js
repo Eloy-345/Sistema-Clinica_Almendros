09ac504a19561d92c08627b2451de623df2b3763
ClinicaAl <114184432+ClinicaAl@users.noreply.github.com>
2022-09-27 22:53:49 -0500

    Update loginR.js

--- routes/loginR.js
+++ routes/loginR.js
@@ -5,44 +5,45 @@ const connection = require('../database/conexiondb')
 // ES6 Modules or TypeScript
 // CommonJS
 const Swal = require('sweetalert2')
-router.post('/auth', function (request, response) {
+router.post('/auth', function (req, res) {
 	// Capture the input fields
-	let username = request.body.username;
-	let password = request.body.password;
+	let username = req.body.username;
+	let password = req.body.password;
 	// Ensure the input fields exists and are not empty
 	if (username && password) {
 		// Execute SQL query that'll select the account from the database based on the specified username and password
-		connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
+		connection.query('SELECT * FROM user1 WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
 			// If there is an issue with the query, output the error
 			if (error) throw error;
 			// If the account exists
 			if (results.length > 0) {
 				// Authenticate the user
-				request.session.loggedin = true;
-				request.session.username = username;
+				req.session.loggedin = true;
+				req.session.username = username;
 				// Redirect to home page
-				response.redirect('/home1');
+				res.redirect('/home1');
 			} else {
-				response.redirect('/');
+				req.flash('danger', 'Flash is back!')
+				res.redirect('/');
 			}
-			response.end();
+			res.end();
 		});
 	} else {
-		response.send('Por favor ingresa Usuario y Contraseña!');
-		response.end();
+		res.send('Por favor ingresa Usuario y Contraseña!');
+		res.end();
 	}
 });
 // http://localhost:3000/home
-router.get('/home1', function (request, response,next) {
+router.get('/home1', function (req, res,next) {
 	// If the user is loggedin
-	if (request.session.loggedin) {
+	if (req.session.loggedin) {
 		// Output username
 		return next()
 	} else {
 		// Not logged in
-		response.redirect('/')
+		res.redirect('/')
 	}
-	response.end();
+	res.end();
 });
 
 module.exports = router                  
